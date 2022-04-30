40ba9596bb97490d2646ef70d80318a5d77d4532
RomStark <rgtarkovskiy@edu.hse.ru>
2022-04-30 13:39:07 +0300

    Initial Commit

---
+++ HSE_Apple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSE_Apple
+//
+//  Created by Al Stark on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
